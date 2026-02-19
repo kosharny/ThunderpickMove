344877cf3b49d3a5c0f5d5d03888e258343c268d
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-19 18:23:51 +0200

    Initial Commit

---
+++ ThunderpickMove/ThunderpickMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderpickMoveApp.swift
+//  ThunderpickMove
+//
+//  Created by Maksim Kosharny on 19.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ThunderpickMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
